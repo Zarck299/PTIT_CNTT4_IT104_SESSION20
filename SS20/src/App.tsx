1b9a790eeb86e41c4340e838d55a77cc3e8ef7ef
Anh <honganhzarck@gmail.com>
2025-09-01 23:08:20 +0700

    SS20

--- SS20/src/App.tsx
+++ SS20/src/App.tsx
@@ -1,13 +1,13 @@
 import './App.css'
-import CheckInput from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE01/Bai1';
-import UserProfile from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE02/Bai2';
-import Welcome from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE03/Bai3';
-import PageTitle from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE04/Bai4';
-import Timer from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE05/Bai5';
-import FocusInput from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE06/Bai6';
-import Count from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE07/Bai7';
-import UserForm from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE08/Bai8';
-import Search from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE09/Bai9';
+// import CheckInput from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE01/Bai1';
+// import UserProfile from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE02/Bai2';
+// import Welcome from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE03/Bai3';
+// import PageTitle from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE04/Bai4';
+// import Timer from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE05/Bai5';
+// import FocusInput from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE06/Bai6';
+// import Count from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE07/Bai7';
+// import UserForm from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE08/Bai8';
+// import Search from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE09/Bai9';
 export default function App() {
   return(
     <>
