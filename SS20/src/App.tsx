dcc3bd3da1522661e41129719298a54586333c41
Anh <honganhzarck@gmail.com>
2025-09-01 23:07:16 +0700

    SS20

--- SS20/src/App.tsx
+++ SS20/src/App.tsx
@@ -1,27 +1,25 @@
 import './App.css'
-// import ShoppingCart from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE01/Bai1';
-// import UserProfile from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE02/Bai2';
-// import Welcome from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE03/Bai3';
-// import SimpleForm from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE04/Bai4';
-// import Slogan from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE05/Bai5';
-// import KeyTracker from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE06/Bai6';
-// import ScrollSection from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE07/Bai7';
-// import LoginForm from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE08/Bai8';
-// import AppLanguageChange from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE09/Bai9';
-// import ItemList from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE10/Bai10';
+import CheckInput from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE01/Bai1';
+import UserProfile from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE02/Bai2';
+import Welcome from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE03/Bai3';
+import PageTitle from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE04/Bai4';
+import Timer from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE05/Bai5';
+import FocusInput from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE06/Bai6';
+import Count from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE07/Bai7';
+import UserForm from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE08/Bai8';
+import Search from './component/PTIT_CNTT4_IT104_SESSION20_EXERCISE09/Bai9';
 export default function App() {
   return(
     <>
-    {/* <ShoppingCart></ShoppingCart> */}
+    {/* <CheckInput></CheckInput> */}
     {/* <UserProfile></UserProfile> */}
     {/* <Welcome></Welcome> */}
-    {/* <SimpleForm></SimpleForm> */}
-    {/* <Slogan></Slogan> */}
-    {/* <KeyTracker></KeyTracker> */}
-    {/* <ScrollSection></ScrollSection> */}
-    {/* <LoginForm></LoginForm> */}
-    {/* <AppLanguageChange></AppLanguageChange> */}
-    {/* <ItemList></ItemList> */}
+    {/* <PageTitle></PageTitle> */}
+    {/* <Timer></Timer> */}
+    {/* <FocusInput></FocusInput> */}
+    {/* <Count></Count> */}
+    {/* <UserForm></UserForm> */}
+    {/* <Search></Search> */}
     </>
   )  
 }
